e7ed12231e915cd5ea78e8fcb3dc4af10e33b64c
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2022-08-29 14:45:34 -0300

    add linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import {Button, VStack, HStack,  Text, Image} from '@chakra-ui/react'
-import { useState } from 'react'
-import vite from '/vite.svg'
-import react from '/react.svg'
-import electron from '/electron.png'
+import { Button, VStack, HStack, Text, Image } from '@chakra-ui/react';
+import { useState } from 'react';
+import vite from '/vite.svg';
+import react from '/react.svg';
+import electron from '/electron.png';
 
 export default function App() {
-const [count, setCount] = useState(0)
-  return (
-      <>
-      <VStack spacing={4} align='center' pt={12}>
-        <HStack>
-          <Image src={vite} alt='vite logo' boxSize='150px' />
-          <Image src={react} alt='vite logo' boxSize='150px' />
-          <Image src={electron} alt='vite logo' boxSize='150px' />
-        </HStack>
-          <Button color='blue.200' onClick={() => setCount(count + 1)} >Count</Button>
-          <Text>count is {count}</Text>
-      </VStack>
-      </>
-  )
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <VStack align="center" pt={12} spacing={4}>
+                <HStack>
+                    <Image alt="vite logo" boxSize="150px" src={vite} />
+                    <Image alt="vite logo" boxSize="150px" src={react} />
+                    <Image alt="vite logo" boxSize="150px" src={electron} />
+                </HStack>
+                <Button color="blue.200" onClick={() => setCount(count + 1)}>
+                    Count
+                </Button>
+                <Text>count is {count}</Text>
+            </VStack>
+        </>
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ChakraProvider } from '@chakra-ui/react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App /> 
-    </ChakraProvider>
-  </React.StrictMode>
-)
-
-postMessage({ payload: 'removeLoading' }, '*')
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>
+);
